5d5027f37851ab292e5ee6ac1a67594a4c60f883
gonzalezmaxi862 <gonzalezmaxi862@gmail.com>
2023-04-18 18:40:08 -0300

    Cosasnuevas

--- assets/scenes/Game.js
+++ assets/scenes/Game.js
@@ -21,11 +21,32 @@ export default class Game extends Phaser.Scene {
   preload() {
     // load assets
     this.load.image("sky", "./assets/images/Cielo.png");
+    this.load.image("platform", "./assets/images/platform.png");
+    this.load.image("Player", "./assets/images/Ninja.png");
+    this.load.image("triangulo", "./assets/images/Triangulo.png");
   }
 
   create() {
     // create game objects
-    this.add.image(400, 300, "sky").setScale(0.555).refreshBody();
+    this.add.image(400, 300, "sky").setScale(0.555);
+    // jugador
+    this.Player = this.physics.add.sprite(100, 200, "Player");
+    // grupo formas
+    this.ShapeGroup = this.physics.add.group();
+    this.ShapeGroup.create(100, 0, "triangulo");
+
+    // grupo plataformas
+    this.plataformasPropias = this.physics.add.staticGroup();
+    this.plataformasPropias
+      .create(400, 550, "platform")
+      .setScale(2)
+      .refreshBody();
+    // colicion del PJ con suelo
+    this.physics.add.collider(this.Player, this.plataformasPropias);
+    // colicion entre suelo y formas
+    this.physics.add.collider(this.ShapeGroup, this.plataformasPropias);
+    // overlap entre formas y Pj
+    this.physics.add.overlap(this.Player, this.ShapeGroup);
   }
 
   update() {
